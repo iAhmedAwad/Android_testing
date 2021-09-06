dcf9cbc6972a74d1c5bb5e2ea57c58b8a67a84a7
Ahmed Awad <ahmed.mmawad@hotmail.com>
2021-09-06 14:55:58 +0200

    Providing Room database

--- app/src/main/java/com/trianglz/common/Constants.kt
+++ app/src/main/java/com/trianglz/common/Constants.kt
@@ -2,7 +2,8 @@ package com.trianglz.common
 
 class Constants {
     companion object {
-    const    val ID_KEY: String = "id_key"
+        const val DATABASE_NAME = "horses_db"
+        const val ID_KEY: String = "id_key"
         const val BASE_URL = "https://jsonplaceholder.typicode.com/"
         const val PAGE_NUMBER = "page"
         const val USERS_PATH = "users"
